4ded9cf7bdc6e221d5f3bf44f1b0ce6cff862677
Layicovaaliya <162481015+Layicovaaliya@users.noreply.github.com>
2024-06-19 16:35:49 +0400

    filter/map

---
+++ app.js
@@ -0,0 +1,15 @@
+// let a = [3, 4, 5];
+// let b = a.map(number=> number*number);
+// console.log(b);
+
+
+// let arr = [
+//      ['alma','qırmızı'],
+//      ['alça','yaşıl'],
+//      ['gilas','qırmızı'],
+//      ['heyva','sarı'],
+// ];
+// let qırmızı = arr.filter(user => user[1]=="qırmızı");
+// console.log(qırmızı);
+
+
